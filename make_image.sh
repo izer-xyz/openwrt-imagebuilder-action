ade0cbadbaa6c1cf80e0e1d0e674b11a2701cf14
Péter <peter@perenyi.net>
2023-01-09 23:12:32 +1100

    Update make_image.sh

--- make_image.sh
+++ make_image.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #set
+sed -e '/^CONFIG_TARGET_ROOTFS_EXT4FS/ s/^#*/#/' -i .config
 
-#cat .config
+cat .config
+
+ls -la files
 
 make image FILES="$FILES" PACKAGES="$PACKAGES" DISABLED_SERVICES="$DISABLED_SERVICES"
