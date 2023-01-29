09972d2700f8df58ce28804b001979888a6a9503
Péter <peter@perenyi.net>
2023-01-29 23:24:18 +1100

    Create symlink of outputs

--- make_image.sh
+++ make_image.sh
@@ -15,3 +15,10 @@ make image \
   PACKAGES="$PACKAGES" \
   DISABLED_SERVICES="$DISABLED_SERVICES" \
   EXTRA_IMAGE_NAME="$EXTRA_IMAGE_NAME"
+
+mkdir bin/target/
+
+for file in bin/targets/*/*/openwrt-$OPENWRT_VERSION-$PROFILE-*
+do 
+  ln -s  "$file" "bin/target/${file#bin/targets/*/*/openwrt-$OPENWRT_VERSION-$PROFILE-}"
+done
