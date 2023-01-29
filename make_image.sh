ec288e7ec49a0e723bb2d8ffac944892291c17cf
Péter <peter@perenyi.net>
2023-01-29 23:49:10 +1100

    Update make_image.sh

--- make_image.sh
+++ make_image.sh
@@ -18,7 +18,7 @@ make image \
 
 mkdir bin/target/
 
-for file in bin/targets/*/*/openwrt-$OPENWRT_VERSION-$OPENWR_PROFILE-*
+for file in bin/targets/*/*/*
 do 
   echo ln -s  "$file" "bin/target/${file#bin/targets/*/*/openwrt-*-*-*-}"
 done
