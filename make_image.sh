c3650ec7f1cf1cb873e9f7d3de79c749ec257529
Péter <peter@perenyi.net>
2023-01-30 21:34:57 +1100

    Cleanup bin folder

--- make_image.sh
+++ make_image.sh
@@ -13,12 +13,17 @@ sed \
 make image \
   FILES="$FILES" \
   PACKAGES="$PACKAGES" \
-  DISABLED_SERVICES="$DISABLED_SERVICES" \
-  EXTRA_IMAGE_NAME="$EXTRA_IMAGE_NAME"
+  DISABLED_SERVICES="$DISABLED_SERVICES"
 
 mkdir bin/target/
 
 for file in bin/targets/*/*/openwrt-*
 do 
-  mv  "$file" "bin/target/${file#bin/targets/*/*/openwrt-*-*-*-}"
+  # TODO There might be a bug with the "openwrt-*-*-*-" pattern if not all profile match this format
+  mv  "$file" "bin/${file#bin/targets/*/*/openwrt-*-*-*-}"
 done
+
+mv bin/targets/*/*/* bin
+rm -rf bin/targets
+
+ls -lah bin/
